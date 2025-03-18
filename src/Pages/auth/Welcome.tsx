9b5221f9b6aa24bf3ea43fb95efa4b970afabbb0
FeMarquesSilva <felipemarques330@gmail.com>
2025-03-18 19:30:17 -0300

    Ajustando a mesclagem de camadas

--- src/Pages/auth/Welcome.tsx
+++ src/Pages/auth/Welcome.tsx
@@ -1,33 +1,19 @@
-<<<<<<< HEAD
-import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
-import Logo from "../../imgs/logo.png";
-=======
 import { Box, Button, Flex, Image, Input, Text } from "@chakra-ui/react";
-import Logo from "../../imgs/logo.png"
->>>>>>> f4a4c9ee67505c4c025e7131067d3f5bfe8fa1bb
+import Logo from "../../imgs/logo.png";
 
 const Welcome = () => {
   return (
     <Box>
-<<<<<<< HEAD
-      <Heading>
-        <Flex justifyContent={"center"}>
-          <Image src={Logo} justifyContent={"center"} />
-        </Flex>
-      </Heading>
+      <Flex justifyContent={"center"}>
+        <Image src={Logo} justifyContent={"center"} />
+      </Flex>
+
+      <Flex justifyContent={"center"}>
+        <Image src={Logo} />
+      </Flex>
       <Text textAlign={"center"}>
         Gerencie sua transportadora de forma eficiente e profissional
       </Text>
-    </Box>
-  );
-};
-=======
-      <Flex justifyContent={"center"}>
-        <Image
-          src={Logo}
-        />
-      </Flex>
-      <Text textAlign={"center"}>Gerencie sua transportadora de forma eficiente e profissional</Text>
       <Flex justifyContent={"center"}>
         <Flex flexDir={"column"} gap={2} boxSize={450}>
           <Button>Entrar</Button>
@@ -36,7 +22,6 @@ const Welcome = () => {
       </Flex>
     </Box>
   );
-}
->>>>>>> f4a4c9ee67505c4c025e7131067d3f5bfe8fa1bb
+};
 
 export default Welcome;
