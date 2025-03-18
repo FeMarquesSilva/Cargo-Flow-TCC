f4a4c9ee67505c4c025e7131067d3f5bfe8fa1bb
FeMarquesSilva <felipemarques330@gmail.com>
2025-03-17 23:03:04 -0300

    Adicionando os botões de login/register

--- src/Pages/auth/Welcome.tsx
+++ src/Pages/auth/Welcome.tsx
@@ -1,11 +1,23 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Image, Input, Text } from "@chakra-ui/react";
+import Logo from "../../imgs/logo.png"
 
 const Welcome = () => {
-    return (
-      <Box>
-        <Text>Bem-vindo ao seu projeto React com TypeScript!</Text>
-      </Box>
-      );
+  return (
+    <Box>
+      <Flex justifyContent={"center"}>
+        <Image
+          src={Logo}
+        />
+      </Flex>
+      <Text textAlign={"center"}>Gerencie sua transportadora de forma eficiente e profissional</Text>
+      <Flex justifyContent={"center"}>
+        <Flex flexDir={"column"} gap={2} boxSize={450}>
+          <Button>Entrar</Button>
+          <Button>Criar Conta</Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 }
 
 export default Welcome;
